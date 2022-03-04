b62307c70d3b3aa7619fd3f84e2dddea2341a0e9
MD ALISHAN ALI <92203534+mdalishanali@users.noreply.github.com>
2022-03-04 19:03:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="cont">
+      <h1 className="note-title">Notes Making</h1>
+      <div className="main">
+        <input type="text" className="add-note"></input>
+        <input type="submit" />
+      </div>
+
+      <div className="note-title">
+        <li>aldfj</li>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Input.jsx
@@ -0,0 +1,14 @@
+var  Noteslist= ()=>{
+    <div className="cont">
+    <h1 className="note-title">Notes Making</h1>
+    <div className="main">
+      <input type="text" className="add-note"></input>
+      <input type="submit" />
+    </div>
+
+    <div className="note-title">
+      <li>aldfj</li>
+    </div>
+  </div>
+}
+export {Noteslist};
