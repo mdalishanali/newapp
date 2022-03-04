a72be1ae43dd19c16a4a96c72f270c8dc3144f64
MD ALISHAN ALI <92203534+mdalishanali@users.noreply.github.com>
2022-03-04 19:15:34 +0530

    alishant commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="cont">
+      <h1>ajdfk</h1>
       <h1 className="note-title">Notes Making</h1>
       <div className="main">
         <input type="text" className="add-note"></input>
